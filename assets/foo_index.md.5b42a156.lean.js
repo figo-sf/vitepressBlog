30d6d056203e454a2a1f07d032b25e7f7880a586
figo-sf <figo-sf@users.noreply.github.com>
2022-09-29 10:54:09 +0000

    deploy: 3053f423226569abfbb47ea736535309416ab1d8

---
+++ assets/foo_index.md.5b42a156.lean.js
@@ -0,0 +1,1 @@
+import{_ as o,o as e,c as a,d}from"./app.ddefc3f4.js";const x=JSON.parse('{"title":"hello word foo","description":"","frontmatter":{},"headers":[],"relativePath":"foo/index.md"}'),r={name:"foo/index.md"},h=d("",8),l=[h];function i(n,t,f,s,c,_){return e(),a("div",null,l)}const u=o(r,[["render",i]]);export{x as __pageData,u as default};
